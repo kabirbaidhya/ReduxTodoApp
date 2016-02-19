b8aba64762177a2b278ae14f9b7c12bbd83f0d1e
Kabir <kabeer182010@gmail.com>
2016-02-19 10:56:07 +0545

    Try less verbose syntax

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,18 +1,16 @@
 
 import React from 'react';
 import Item from './Item.jsx';
-import Component from './base/Component';
 
-class List extends Component {
-    render() {
-        return (
-            <ul className="todo-list">
-                {this.props.todos.map((item, index) =>
-                    <Item key={item.id} {...item} />
-                )}
-            </ul>
-        );
-    }
+function List ({todos}) {
+    console.log('List render');
+    return (
+        <ul className="todo-list">
+            {todos.map((item, index) =>
+                <Item key={item.id} {...item} />
+            )}
+        </ul>
+    );
 }
 
 export default List;
