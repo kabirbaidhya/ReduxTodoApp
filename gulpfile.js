80ee039ad1ba6f3e617e31f57dba62319e8af19c
Kabir <kabeer182010@gmail.com>
2016-02-21 14:10:34 +0545

    Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var webpack = require('webpack-stream');
+var cssnano = require('gulp-cssnano');
 
 // Webpack
 gulp.task('webpack', function() {
@@ -9,18 +11,35 @@ gulp.task('webpack', function() {
 
     return gulp.src('src/app.js')
         .pipe(webpack(config))
-        .pipe(gulp.dest('public/build'));
+        .pipe(gulp.dest('public/build/js'));
 });
 
 gulp.task('webpack-uglify', ['webpack'], function() {
-    gulp.src('public/build/bundle.js')
+    gulp.src('public/build/js/bundle.js')
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
-        .pipe(gulp.dest('public/build'));
+        .pipe(gulp.dest('public/build/js'));
 });
 
-gulp.task('default', ['webpack', 'webpack-uglify']);
+gulp.task('sass', function() {
+    gulp.src('./resources/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/build/css'));
+});
+
+gulp.task('sass-uglify', ['sass'], function() {
+    gulp.src('public/build/css/app.css')
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('public/build/css'));
+});
+
+gulp.task('default', [
+    'webpack', 'webpack-uglify',
+    'sass', 'sass-uglify'
+]);
 
 gulp.task('watch', ['default'], function() {
-    gulp.watch('src/**/*.js*', ['default']);
+    gulp.watch('src/**/*.js*', ['webpack', 'webpack-uglify']);
+    gulp.watch('resources/**/*.scss', ['sass', 'sass-uglify']);
 });
